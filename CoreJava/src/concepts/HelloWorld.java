9cbf8243178899cb1782040e58602faaf6455bfc
vinay <vinaysahrawat183a@gmail.com>
2024-12-07 17:45:37 +0530

    added primitive type examples

--- CoreJava/src/concepts/HelloWorld.java
+++ CoreJava/src/concepts/HelloWorld.java
@@ -15,6 +15,8 @@ public class HelloWorld {
         // Non-Primitive Data Types
         String name = "John"; // String
         int[] marks = {85, 90, 78}; // Array
+        Integer[] marx = {1, 2, 3};
+        String[] names = {"HI", "Hello", "Listen"};
         
         System.out.println("Age: " + age);
         System.out.println("Salary: " + salary);
@@ -22,6 +24,8 @@ public class HelloWorld {
         System.out.println("Is Employed: " + isEmployed);
         System.out.println("Name: " + name);
         System.out.println("Marks: " + marks[0] + ", " + marks[1] + ", " + marks[2]);
+        System.out.println("Marks: " + marx[0] + ", " + marx[1] + ", " + marx[2]);
+        System.out.println("Marks: " + names[0] + ", " + names[1] + ", " + names[2]);
 	}
 
 }
